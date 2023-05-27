25d9b2a0053bed58ae61ccd9850774f94209de73
jorge <jorgefco_rf@hotmail.com>
2023-05-27 08:32:16 -0700

    Tercera entrega

--- boot.py
+++ boot.py
@@ -2,25 +2,162 @@ import socket
 import gc
 import network
 from dcmotor import DCMotor
+from dcmotor import Servo
+from dcmotor import HCSR04
 from machine import Pin, PWM
 from time import sleep
+import dht
+
+
+def control(data):
+    distancia = sensor.distance_cm()
+    print(distancia)
+
+    if 2 < distancia < 60:
+        print("Distancia muy corta")
+        motor_traction.stop()
+        motor_traction.backwards(100)
+        sleep(0.3)
+        motor_traction.stop()
+        return
+
+    try:
+
+        accel, reverse, direction, headlight, demo = data.split(':')
+
+        if data != "":
+            direction = int(direction)
+            accel = int(accel)
+            reverse = int(reverse)
+            headlight = int(headlight)
+            demo = int(demo)
+
+            if demo == 1:
+                demonstration()
+                return
+
+            if headlight == 1:
+                headlights_1.duty(1023)  # 1024 = 100%
+                headlights_2.duty(1023)
+            else:
+                headlights_1.duty(100)
+                headlights_2.duty(100)
+
+            if -30 < direction < 30:
+                motor_direction.stop()
+                servom.move(95)
+            else:
+                if direction >= 0:
+                    print("Derecha")
+                    motor_direction.forward(direction + 30)
+                    servom.move(50)
+                else:
+                    print("Izquierda")
+                    motor_direction.backwards((-1 * direction) + 30)
+                    servom.move(140)
+
+            if accel > 0 and reverse == 0:
+                print("Avanzando")
+                motor_traction.forward(accel)
+
+            elif accel == 0 and reverse > 0:
+                # Prender led reversa
+                print("Retrocediendo")
+                motor_traction.backwards(reverse)
+            else:
+                motor_traction.stop()
+
+    except Exception as e:
+        print(f"Data erronea: {data} {e}")
+        return
+
+        # conn.sendall(data)
+
+def demonstration():
+    #Movemos servo
+    servom.move(90)
+    sleep(0.2)
+    servom.move(0)
+
+    # Cambio de luces
+
+    for i in range(0, 5):
+        headlights_1.duty(1023)  # 1024 = 100%
+        headlights_2.duty(1023)
+        sleep(1)
+        headlights_1.duty(100)  # 1024 = 100%
+        headlights_2.duty(100)
+        sleep(1)
+
+
+    #Movemos direccion
+    motor_direction.backwards(100)
+    sleep(0.5)
+    motor_direction.forward(100)
+    sleep(0.5)
+    motor_direction.backwards(100)
+    sleep(0.5)
+    motor_direction.forward(100)
+    sleep(0.5)
+    motor_direction.backwards(100)
+    sleep(0.5)
+    motor_direction.forward(100)
+    sleep(0.5)
+    motor_direction.backwards(100)
+    sleep(0.5)
+    motor_direction.forward(100)
+    sleep(0.5)
+
+    #avanzar y retroceder
+    motor_traction.forward(80)
+    sleep(2)
+    motor_traction.stop()
+    motor_traction.backwards(80)
+    sleep(2)
+
+    #Circulos a la izquierda
+    motor_direction.backwards(100)
+    motor_traction.forward(100)
+    sleep(3)
+    motor_direction.stop()
+    motor_direction.backwards(100)
+    sleep(3)
+
+    print("Finalizando demo.")
 
 
 # Socket
 HOST = ""
 PORT = 5000
 
-#AP
-ssid = 'El carrito follador'
-password = '12345678'
+# Ultrasonico
+sensor = HCSR04(trigger_pin=23, echo_pin=22, echo_timeout_us=10000)
 
+headlights_1 = PWM(Pin(12), freq=5000)
+headlights_1.init()
+headlights_1.duty(1023)
+
+headlights_2 = PWM(Pin(13), freq=5000)
+headlights_2.init()
+headlights_2.duty(1023)
+
+#Servo
+servom = Servo(pin=15)
+
+#DHT
+d = dht.DHT11(Pin(2))
+
+# AP
+ssid = 'El carrito follador'
+
+print(d.temperature(), "grados") # eg. 23 (°C)
 print("Setting up AP...")
 
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
 ap.config(essid=ssid)
 
-#MOTORS
+# MOTORS
 
 print("Setting up motors...")
 
@@ -35,7 +172,6 @@ pin4 = Pin(19, Pin.OUT)
 enable_traction = PWM(Pin(18), frequency)
 motor_traction = DCMotor(pin3, pin4, enable_traction, 350, 1023)
 
-
 print("Starting socket...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 s.bind((HOST, PORT))
@@ -43,34 +179,18 @@ s.listen()
 
 print("Socket started.")
 
-gc.collect()
-conn, addr = s.accept()
+gc.collect()  # Limpia memoria
+conn, addr = s.accept()  # Aceptanmos la conexion
 
 while True:
-    #We will receive a string like "x.xx:y.yy", totaling 9 bytes
-    data = conn.recv(11).decode()
-    print(data)
-    accel, reverse, direction = data.split(':')
-    sleep(0.1)
-    if data != "":
-        direction = int(direction)
-        accel = int(accel)
-        reverse = int(reverse)
+    data = conn.recv(15).decode()
+    control(data)
 
-        if -30 < direction < 30:
-            motor_direction.stop()
-        else:
-            if direction >= 0:
-                motor_direction.forward(direction+30)
-            else:
-                motor_direction.backwards((-1*direction)+30)
-
-        if accel > 0 and reverse == 0:
-            motor_traction.forward(accel)
-
-        elif accel == 0 and reverse > 0:
-            motor_traction.backwards(reverse)
-        else:
-            motor_traction.stop()
-
-        #conn.sendall(data)
+    try:
+        d.measure()
+        #sleep(3)
+        temperature = str(d.temperature())
+        print(f"Temp is -> {temperature}")
+        conn.send(temperature.encode())
+    except:
+        conn.send("25".encode())
--- dcmotor.py
+++ dcmotor.py
@@ -1,3 +1,7 @@
+from machine import Pin, PWM
+import machine
+import time
+
 class DCMotor:
     def __init__(self, pin1, pin2, enable_pin, min_duty=750, max_duty=1023):
         self.speed = 0
@@ -34,3 +38,116 @@ class DCMotor:
         else:
             duty_cycle = int(self.min_duty + (self.max_duty - self.min_duty) * ((self.speed - 1) / (100 - 1)))
         return duty_cycle or 0
+
+class Servo:
+    # these defaults work for the standard TowerPro SG90
+    __servo_pwm_freq = 50
+    __min_u10_duty = 26 - 0 # offset for correction
+    __max_u10_duty = 123- 0  # offset for correction
+    min_angle = 0
+    max_angle = 180
+    current_angle = 0.001
+
+
+    def __init__(self, pin):
+        self.__initialise(pin)
+
+
+    def update_settings(self, servo_pwm_freq, min_u10_duty, max_u10_duty, min_angle, max_angle, pin):
+        self.__servo_pwm_freq = servo_pwm_freq
+        self.__min_u10_duty = min_u10_duty
+        self.__max_u10_duty = max_u10_duty
+        self.min_angle = min_angle
+        self.max_angle = max_angle
+        self.__initialise(pin)
+
+
+    def move(self, angle):
+        # round to 2 decimal places, so we have a chance of reducing unwanted servo adjustments
+        angle = round(angle, 2)
+        # do we need to move?
+        if angle == self.current_angle:
+            return
+        self.current_angle = angle
+        # calculate the new duty cycle and move the motor
+        duty_u10 = self.__angle_to_u10_duty(angle)
+        self.__motor.duty(duty_u10)
+
+    def __angle_to_u10_duty(self, angle):
+        return int((angle - self.min_angle) * self.__angle_conversion_factor) + self.__min_u10_duty
+
+
+    def __initialise(self, pin):
+        self.current_angle = -0.001
+        self.__angle_conversion_factor = (self.__max_u10_duty - self.__min_u10_duty) / (self.max_angle - self.min_angle)
+        self.__motor = PWM(Pin(pin))
+        self.__motor.freq(self.__servo_pwm_freq)
+
+class HCSR04:
+    """
+    Driver to use the untrasonic sensor HC-SR04.
+    The sensor range is between 2cm and 4m.
+    The timeouts received listening to echo pin are converted to OSError('Out of range')
+    """
+    # echo_timeout_us is based in chip range limit (400cm)
+    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30):
+        """
+        trigger_pin: Output pin to send pulses
+        echo_pin: Readonly pin to measure the distance. The pin should be protected with 1k resistor
+        echo_timeout_us: Timeout in microseconds to listen to echo pin.
+        By default is based in sensor limit range (4m)
+        """
+        self.echo_timeout_us = echo_timeout_us
+        # Init trigger pin (out)
+        self.trigger = Pin(trigger_pin, mode=Pin.OUT, pull=None)
+        self.trigger.value(0)
+
+        # Init echo pin (in)
+        self.echo = Pin(echo_pin, mode=Pin.IN, pull=None)
+
+    def _send_pulse_and_wait(self):
+        """
+        Send the pulse to trigger and listen on echo pin.
+        We use the method `machine.time_pulse_us()` to get the microseconds until the echo is received.
+        """
+        self.trigger.value(0) # Stabilize the sensor
+        time.sleep_us(5)
+        self.trigger.value(1)
+        # Send a 10us pulse.
+        time.sleep_us(10)
+        self.trigger.value(0)
+        try:
+            pulse_time = machine.time_pulse_us(self.echo, 1, self.echo_timeout_us)
+            return pulse_time
+        except OSError as ex:
+            if ex.args[0] == 110: # 110 = ETIMEDOUT
+                raise OSError('Out of range')
+            raise ex
+
+    def distance_mm(self):
+        """
+        Get the distance in milimeters without floating point operations.
+        """
+        pulse_time = self._send_pulse_and_wait()
+
+        # To calculate the distance we get the pulse_time and divide it by 2
+        # (the pulse walk the distance twice) and by 29.1 becasue
+        # the sound speed on air (343.2 m/s), that It's equivalent to
+        # 0.34320 mm/us that is 1mm each 2.91us
+        # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582
+        mm = pulse_time * 100 // 582
+        return mm
+
+    def distance_cm(self):
+        """
+        Get the distance in centimeters with floating point operations.
+        It returns a float
+        """
+        pulse_time = self._send_pulse_and_wait()
+
+        # To calculate the distance we get the pulse_time and divide it by 2
+        # (the pulse walk the distance twice) and by 29.1 becasue
+        # the sound speed on air (343.2 m/s), that It's equivalent to
+        # 0.034320 cm/us that is 1cm each 29.1us
+        cms = (pulse_time / 2) / 29.1
+        return cms
